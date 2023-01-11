8dd06e9806bcfd516a81a8278d7fbd07d69612b7
Jingon-Park <bennp3038@gmail.com>
2023-01-11 22:10:03 +0900

    vue 프로젝트 새로 생성

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
 
-loadFonts()
-
-createApp(App)
-  .use(store)
-  .use(vuetify)
-  .mount('#app')
+createApp(App).use(store).use(router).mount('#app')
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,34 +1,4 @@
-
-module.exports = {
-    outputDir: "../src/main/resources/static/",
-    indexPath: '../templates/index.html',
-
-    pages:{
-      default:{
-        entry: 'src/main.js',
-        template: 'public/index.html',
-        filename: 'index.html'
-      },
-      login:{
-          entry: 'src/pages/login/main.js',
-          template: 'public/login.html',
-          filename: 'login.out.html'
-      },
-      welcome:{
-          entry: 'src/pages/welcome/main.js',
-          template: 'public/welcome.html',
-          filename: 'welcome.out.html'
-      },
-      main:{
-          entry: 'src/pages/main/main.js',
-          template: 'public/main.html',
-          filename: 'main.out.html'
-      }
-  },
-
-    pluginOptions: {
-      vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-    }
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
